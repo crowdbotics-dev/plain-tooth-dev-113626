add915fbbe6ecef48902e3061136bc95e0831ac6
Crowdbotics <engineering@crowdbotics.com>
2023-09-02 01:09:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "plain-tooth-dev-113626",
+  url: "https://plain-tooth-dev-113626.botics.co",
+  api: "https://plain-tooth-dev-113626.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
